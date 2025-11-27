f9eaa09a3eca4615e4227c9b5697311054a28fc2
obapluto-ob <obed.lopeyok@student.moringaschool.com>
2025-11-27 21:25:39 +0300

    Clean up unused deployment files

--- mobile/metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Enable web support
-config.resolver.platforms = ['ios', 'android', 'native', 'web'];
-
-module.exports = config;
